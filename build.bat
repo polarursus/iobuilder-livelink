f215446852e6376027f9fb56f18a4144e1e292d2
polarursus <gyorgy.szeljak@gmail.com>
2023-10-31 09:15:58 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,15 +1,7 @@
 @echo off
 
 :: Setting default values
-SET DEFAULT_UE_PATH=C:\Program Files\Epic Games\UE_5.3
-SET DEFAULT_PLUGIN_PATH=%USERPROFILE%\Documents\IOBuilderLiveLink
+SET UE_PATH=C:\Program Files\Epic Games\UE_5.3
+SET OUTPUT_PATH=%USERPROFILE%\Documents
 
-:: Using arguments if provided, else default values
-
-SET PLUGIN_PATH=%1
-IF "%PLUGIN_PATH%"=="" SET PLUGIN_PATH=%DEFAULT_PLUGIN_PATH%
-
-SET UE_PATH=%2
-IF "%UE_PATH%"=="" SET UE_PATH=%DEFAULT_UE_PATH%
-
-"%UE_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%~dp0IOBuilderLiveLink.uplugin" -package="%PLUGIN_PATH%\IOBuilderLiveLink"
+"%UE_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%~dp0IOBuilderLiveLink.uplugin" -package="%OUTPUT_PATH%\IOBuilderLiveLink"
