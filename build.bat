2ece9346b4d21b5ff3857115a3ad6a9e3da556e2
polarursus <gyorgy.szeljak@gmail.com>
2023-10-31 09:07:29 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,15 @@
-"C:\Program Files\Epic Games\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%~dp0IOBuilderLiveLink.uplugin" -package="C:\Users\DellBear\Documents\Unreal Projects\LiveLinkTest\Plugins\IOBuilderLiveLink"
+@echo off
+
+:: Setting default values
+SET DEFAULT_UE_PATH=C:\Program Files\Epic Games\UE_5.3
+SET DEFAULT_PLUGIN_PATH=%USERPROFILE%\Documents\IOBuilderLiveLink
+
+:: Using arguments if provided, else default values
+
+SET PLUGIN_PATH=%1
+IF "%PLUGIN_PATH%"=="" SET PLUGIN_PATH=%DEFAULT_PLUGIN_PATH%
+
+SET UE_PATH=%2
+IF "%UE_PATH%"=="" SET UE_PATH=%DEFAULT_UE_PATH%
+
+"%UE_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%~dp0IOBuilderLiveLink.uplugin" -package="%PLUGIN_PATH%\IOBuilderLiveLink"
